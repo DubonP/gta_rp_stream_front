765733409d9b4da87b0535c82b6e2b63f3f2081c
Paulo <paulogustavorossi@gmail.com>
2024-01-05 18:10:59 -0300

    tirando /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <iframe
-        src="https://player.twitch.tv/?channel=gaules&parent=gta-rp-stream-front.vercel.app/"
+        src="https://player.twitch.tv/?channel=gaules&parent=gta-rp-stream-front.vercel.app"
         height="350"
         width="350"
         allowFullScreen
