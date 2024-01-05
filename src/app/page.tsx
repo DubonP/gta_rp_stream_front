1905c161be6ec0a1e47caa63085c442c0d4d6607
Paulo <paulogustavorossi@gmail.com>
2024-01-05 17:59:25 -0300

    novo nome de site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <iframe
-        src="https://player.twitch.tv/?channel=gaules&parent=https://gta-rp-stream-front-bt9gmz5f8-dubonp.vercel.app/"
+        src="https://player.twitch.tv/?channel=gaules&parent=https://gta-rp-stream-front.vercel.app/"
         frameBorder="0"
         allowFullScreen={true}
         scrolling="no"
