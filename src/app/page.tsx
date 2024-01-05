1c9de86767504bb5e2c8ae6b7e94c5315945edd0
Paulo <paulogustavorossi@gmail.com>
2024-01-05 18:21:36 -0300

    vendo o mult junto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 export default function Home() {
   return (
-    <div>
-      <iframe
-        src="https://player.twitch.tv/?channel=gaules&parent=gta-rp-stream-front.vercel.app"
-        height="350"
-        width="350"
-        allowFullScreen
-      ></iframe>
-    </div>
+    <>
+      <div>
+        <iframe
+          src="https://player.twitch.tv/?channel=gaules&parent=gta-rp-stream-front.vercel.app"
+          height="550"
+          width="950"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div>
+        <iframe
+          src="https://www.multitwitch.tv/bisteconee/poderosobagual/luanz7_/paulinholokobr/gaules/luquet4&parent=gta-rp-stream-front.vercel.app"
+          height="550"
+          width="950"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </>
   );
 }
